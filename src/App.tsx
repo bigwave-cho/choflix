5b9bcbed28b26d378d676fc284af413b0009f423
bigwave-cho <hooyaa93@gmail.com>
2023-03-03 21:40:50 +0900

    NavBar :Layout & menu underbar with using  useMatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import './App.css';
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
